ed461efbb12d5b6f9999273e9c145d729020481d
aanikandrov <gepeg3317@mail.ru>
2024-10-29 17:25:48 +0300

    also commit

---
+++ HistoricalBook/HistoricalBook.Service/IoC/SerilogConfigurator.cs
@@ -0,0 +1,23 @@
+﻿using Serilog;
+
+namespace HistoricalBook.Service.IoC;
+
+public static class SerilogConfigurator
+{
+    public static void ConfigureService(WebApplicationBuilder builder)
+    {
+        builder.Host.UseSerilog((context, configuration) =>
+        {
+            configuration
+                .Enrich.WithCorrelationId()
+                .ReadFrom.Configuration(context.Configuration);
+        });
+
+        builder.Services.AddHttpContextAccessor();
+    }
+
+    public static void ConfigureApplication(IApplicationBuilder app)
+    {
+        app.UseSerilogRequestLogging();
+    }
+}
---
+++ HistoricalBook/HistoricalBook.Service/IoC/SwaggerConfigurator.cs
@@ -0,0 +1,16 @@
+﻿namespace HistoricalBook.Service.IoC;
+
+public static class SwaggerConfigurator
+{
+    public static void ConfigureServices(IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+
+    public static void ConfigureApplication(IApplicationBuilder app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+}
