517a8bd3dd2d3464e48a7105edcf9857a0e94a11
aanikandrov <gepeg3317@mail.ru>
2024-11-12 21:39:17 +0300

    lab4 begin

---
+++ HistoricalBook/HistoricalBook.BusinessLogic/Mapper/UserBLProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using HistoricalBook.BusinessLogic.Users.Entities;
+using HistoricalBook.DataAccess.Entities;
+
+namespace HistoricalBook.BusinessLogic.Mapper;
+
+public class UsersBLProfile : Profile
+{
+    public UsersBLProfile()
+    {
+        CreateMap<UserEntity, UserModel>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+            .ForMember(dest => dest.ExternalId, opt => opt.MapFrom(src => src.ExternalId))
+            .ForMember(dest => dest.FullName, 
+                opt => opt.MapFrom(src => $"{src.LastName} {src.FirstName} {src.Patronymic}"));
+        
+        CreateMap<CreateUserModel, UserEntity>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore())
+            .ForMember(dest => dest.ExternalId, opt => opt.Ignore())
+            .ForMember(dest => dest.CreationTime, opt => opt.Ignore())
+            .ForMember(dest => dest.ModificationTime, opt => opt.Ignore());
+    }
+}
---
+++ HistoricalBook/HistoricalBook.BusinessLogic/Users/Entities/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using HistoricalBook.DataAccess.Entities;
+
+namespace HistoricalBook.BusinessLogic.Users.Entities;
+
+public class UserModel
+{
+    public int Id { get; set; }
+    public Guid ExternalId { get; set; }
+    public string Login { get; set; }
+    public string HashPassword { get; set; }
+    public string FullName { get; set; } 
+    public string Email { get; set; }
+    
+    public Role Role { get; set; }
+}
