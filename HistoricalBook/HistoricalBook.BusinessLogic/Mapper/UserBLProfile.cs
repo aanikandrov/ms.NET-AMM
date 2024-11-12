92daf2190344259e3fa5ca6479217b38479fb849
aanikandrov <gepeg3317@mail.ru>
2024-11-12 21:43:02 +0300

    lab4 begin

--- HistoricalBook/HistoricalBook.BusinessLogic/Mapper/UserBLProfile.cs
+++ HistoricalBook/HistoricalBook.BusinessLogic/Mapper/UserBLProfile.cs
@@ -11,8 +11,7 @@ public class UsersBLProfile : Profile
         CreateMap<UserEntity, UserModel>()
             .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
             .ForMember(dest => dest.ExternalId, opt => opt.MapFrom(src => src.ExternalId))
-            .ForMember(dest => dest.FullName, 
-                opt => opt.MapFrom(src => $"{src.LastName} {src.FirstName} {src.Patronymic}"));
+            
         
         CreateMap<CreateUserModel, UserEntity>()
             .ForMember(dest => dest.Id, opt => opt.Ignore())
